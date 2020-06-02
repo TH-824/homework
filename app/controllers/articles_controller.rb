9a1cb89107efd103095a71b7d824defec0e64678
Aranjuez8 <tadashi.in.toyoda@gmail.com>
2020-06-03 06:52:48 +0900

    add update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,34 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+	    @article = Article.find(params[:id])
+	    if @article.update(params.require(:article).permit(:title, :description))
+	    	flash[:notice] = "updated successfully"
+	    	redirect_to @article
+		else
+			render 'edit'
+		end
+			
+	end
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			flash[:notice] = "Article was created sucessufully."
+			redirect_to @article
+		else
+			render 'new'
+		end
+
+	end
+
+
 end
